728c6d05e21182e94d15631f623d895d039f475f
MutiaNandhika <mutianandhika@gmail.com>
2025-05-23 19:34:03 +0700

    profil

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,6 +5,6 @@
     <ul class="nav-links">
         <li><a href="/user">🏠 Beranda</a></li>
         <li><a href="/diagnosa">📋 Diagnosa</a></li>
-        <li><a href="#">👤 Profil</a></li>
+        <li><a href="/profil">👤 Profil</a></li>
     </ul>
 </nav>
