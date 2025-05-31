0c5d805f2f0660fa5b3ef8d0eab335b9ffb66ed1
laniisa <laniisa2207@gmail.com>
2025-05-31 14:18:03 +0700

    fe pakar

--- resources/views/pakar/dashboard.blade.php
+++ resources/views/pakar/dashboard.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.pakar')
 @section('title', 'Dashboard Pakar')
 
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css/pakar/dashboard.css') }}">
+@endpush
+
 @section('content')
 <h1>Welcome Back, {{ Auth::user()->nama }}!</h1>
 <img src="{{ asset('images/logo.png') }}" alt="SOSMEDCARE" class="welcome-logo">
-<p>
+<p >
     Perkembangan teknologi informasi telah mendorong penggunaan media sosial seperti Instagram, TikTok, Facebook, dan X yang kini menjadi bagian dari kehidupan sehari-hari. Meskipun memberikan kemudahan dalam berinteraksi dan berbagi, penggunaan media sosial yang berlebihan dapat menyebabkan kecanduan, terutama pada remaja dan dewasa muda. Dampaknya meliputi penurunan produktivitas, gangguan psikologis, dan menurunnya kualitas hubungan sosial. Sayangnya, banyak orang tidak menyadari bahwa mereka telah kecanduan. Untuk mengatasi hal ini, dapat digunakan sistem pakar berbasis metode Backward Chaining yang mampu mendiagnosis tingkat kecanduan berdasarkan gejala yang dialami pengguna. Sistem ini diharapkan membantu masyarakat mengenali dan mencegah kecanduan media sosial sejak dini.
 </p>
 @endsection
