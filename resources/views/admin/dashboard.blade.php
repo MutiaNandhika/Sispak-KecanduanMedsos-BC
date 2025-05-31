734ee6df101b2abc2f854f1a00ead5d776484502
MutiaNandhika <mutianandhika@gmail.com>
2025-05-31 14:46:04 +0700

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,6 +10,6 @@
 <h1>Welcome Back, Naruto!</h1>
 <img src="{{ asset('images/logo.png') }}" alt="SOSMEDCARE" class="welcome-logo">
 <p>
-    Perkembangan teknologi informasi telah mendorong penggunaan media sosial seperti Instagram, TikTok, Facebook, dan X yang kini menjadi bagian dari kehidupan sehari-hari. Meskipun memberikan kemudahan dalam berinteraksi dan berbagi, penggunaan media sosial yang berlebihan dapat menyebabkan kecanduan, terutama pada remaja dan dewasa muda. Dampaknya meliputi penurunan produktivitas, gangguan psikologis, dan menurunnya kualitas hubungan sosial. Sayangnya, banyak orang tidak menyadari bahwa mereka telah kecanduan. Untuk mengatasi hal ini, dapat digunakan sistem pakar berbasis metode Backward Chaining yang mampu mendiagnosis tingkat kecanduan berdasarkan gejala yang dialami pengguna. Sistem ini diharapkan membantu masyarakat mengenali dan mencegah kecanduan media sosial sejak dini.
+    SosmedCare adalah website berbasis sistem pakar yang membantu pengguna mengetahui tingkat kecanduan mereka terhadap media sosial. Sistem ini bekerja dengan memberikan serangkaian pertanyaan kepada pengguna, di mana setiap pertanyaan mewakili gejala tertentu dari kecanduan media sosial.
 </p>
 @endsection
